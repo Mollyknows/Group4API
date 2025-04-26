fc109cd4bb26d7b122a0674146a141255f7bc5c2
Molly Calhoun <mollyrose.calhoun@gmail.com>
2025-04-26 18:15:19 -0400

    Got the Azure db connected to the client

--- db.js
+++ db.js
@@ -1,16 +1,34 @@
-const mysql = require ('mysql2/promise');
-require ('dotenv').config()
+const sql = require("mssql");
+require("dotenv").config();
 
-const mysqlPool = mysql.createPool({ //connects to the database
-    host: 'localhost',
-    user: process.env.user,
-    password: process.env.password, // username and password for the database are stored in the .env file
-    database: process.env.db_name
+const config = {
+  server: "group4gimpsql.database.windows.net",
+  database: "Group4GimpDB",
+  user: "group4admin",
+  password: "KSU2025group4",
+  options: {
+    trustedConnection: true,
+    enableArithAbort: true,
+    trustServerCertificate: true,
+  },
+};
 
-});
+module.exports = {
+  connect: () => sql.connect(config),
+  sql,
+};
 
-mysqlPool.query("SELECT 1")
-    .then(() => console.log('Successful Database Connection'))
-    .catch(err => console.log('Database Connection Failed. \n' + err));
+// const mysqlPool = mysql.createPool({ //connects to the database
+//     host: 'group4gimpsql.database.windows.net',
+//     user: process.env.user,
+//     password: process.env.password, // username and password for the database are stored in the .env file
+//     database: process.env.db_name
 
-module.exports = mysqlPool;
+// });
+
+// mysqlPool
+//   .query("SELECT 1")
+//   .then(() => console.log("Successful Database Connection"))
+//   .catch((err) => console.log("Database Connection Failed. \n" + err));
+
+// module.exports = mysqlPool;
--- index.js
+++ index.js
@@ -1,91 +1,104 @@
-const express = require ('express');
-    app = express();
-    bodyParser = require('body-parser');
+const express = require("express");
+const app = express();
 
-require('express-async-errors');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { connect } = require("./db.js");
 
-const db = require ('./db'),
-    extensionRoutes = require('./controllers/extension.controller');
-    authRoutes = require('./controllers/auth.controller');
+// app.use(express.static(path.join(__dirname, "js")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
+
+connect()
+  .then((connection) => {
+    console.log("Connected to the database.");
+  })
+  .catch((error) => {
+    console.log("Database connection failed!");
+  });
+
+//removed this to set up azure db connection
+// require('express-async-errors');
+
+// const db = require("./db"),
+//   extensionRoutes = require("/controllers/extension.controller.js");
+// authRoutes = require("/controllers/auth.controller.js");
 
 //middleware
 app.use(bodyParser.json());
-app.use('/api', apiRoutes);
+// app.use("/api", apiRoutes);
 app.use(express.json());
-app.use(express.static('public')); //folder for static files
-
+app.use(express.static("public")); //folder for static files
 
 // Front-end routes
 
 // Home page
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // Extensions gallery page
-app.get('/gallery', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'gallery.html'));
+app.get("/gallery", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "gallery.html"));
 });
 
 // Extension detail page
-app.get('/extension/:id', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'extension-detail.html'));
+app.get("/extension/:id", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "extension-detail.html"));
 });
 
 // Upload extension page (requires authentication)
-app.get('/upload', (req, res) => {
-    if (!req.session.user) {
-        return res.redirect('/login?redirect=/upload');
-    }
-res.sendFile(path.join(__dirname, 'public', 'upload.html'));
+app.get("/upload", (req, res) => {
+  if (!req.session.user) {
+    return res.redirect("/login?redirect=/upload");
+  }
+  res.sendFile(path.join(__dirname, "public", "upload.html"));
 });
 
 // Manage extensions page (requires authentication)
-app.get('/manage', (req, res) => {
-    if (!req.session.user) {
-        return res.redirect('/login?redirect=/manage');
-    }
+app.get("/manage", (req, res) => {
+  if (!req.session.user) {
+    return res.redirect("/login?redirect=/manage");
+  }
 
-    res.sendFile(path.join(__dirname, 'public', 'manage.html'));
+  res.sendFile(path.join(__dirname, "public", "manage.html"));
 });
 
 // Login page
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "login.html"));
 });
 
 // Register page
-app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'register.html'));
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "register.html"));
 });
 
 // Documentation page
-app.get('/docs', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'documentation.html'));
+app.get("/docs", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "documentation.html"));
 });
 
 // Search page
-app.get('/search', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'search.html'));
+app.get("/search", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "search.html"));
 });
 
 // User profile page
-app.get('/profile', (req, res) => {
-    if (!req.session.user) {
-        return res.redirect('/login?redirect=/profile');
-    }
-    res.sendFile(path.join(__dirname, 'public', 'profile.html'));
+app.get("/profile", (req, res) => {
+  if (!req.session.user) {
+    return res.redirect("/login?redirect=/profile");
+  }
+  res.sendFile(path.join(__dirname, "public", "profile.html"));
 });
-
-
 
 //error handeling
 app.use((err, req, res, next) => {
-    console.log(err);
-    res.status(err.status || 500).send('Something went wrong!');
+  console.log(err);
+  res.status(err.status || 500).send("Something went wrong!");
 });
 
-
 //Starts application on localhost port 3000
-app.listen(3000,
-    () => console.log('Server Started on port 3000'));
+app.listen(3000, () => console.log("Server Started on port 3000"));
